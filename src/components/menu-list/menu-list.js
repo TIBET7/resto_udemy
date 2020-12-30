e3a9d714fac7acc04ed455968fd111c4756ae06d
Dmitriy <dmitrinit@gmail.com>
2020-12-30 18:00:57 +0300

    add app structure

---
+++ src/components/menu-list/menu-list.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import MenuListItem from '../menu-list-item';
+
+import './menu-list.scss';
+
+class MenuList extends Component {
+
+    render() {
+
+        const { menuItems } = this.props;
+
+        return (
+            <ul className="menu__list">
+                {
+                menuItems.map((menuItem) => {
+                        return <MenuListItem key={menuItem.id} menuItem={menuItem}/>
+                    })
+                }
+            </ul>
+        )
+    }
+};
+
+
+export default MenuList;
